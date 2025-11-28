4026f896b89d764371ad01528b3aa02b61bf3d0e
joinvikram07-byte <joinvikram07@gmail.com>
2025-11-28 08:24:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Replace with your deployed Google Apps Script web app URL
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw_pZf0VgNVHafTEAFCcEa-NirOHjDlh_5WS8VTaWiAGC2ZXkUK-GTrga96xG9lDjKKJQ/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/a/macros/medcaretrust.com/s/AKfycbw5Qq6fhMXQx5UxriDyR_zTSlWvG7vjENblzLEW8FzIFU1cXgbsQRgDHgW3kVzisS56Ew/exec";
 
 // Support/WhatsApp number to open after successful submit (no +, country code included)
 const SUPPORT_WA = "919266023555";
